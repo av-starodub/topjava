5f43d146f2a12daaf3c658b9a86490896085da5e
Alexandr Starodubenko <alexvladstr@gmail.com>
2023-06-10 02:30:18 +0300

    Implement InMemoryUserRepository class

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -14,7 +14,15 @@ public class SpringMain {
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
+            adminUserController.create(new User(null, "userNameB", "emailB@mail.ru", "password", Role.ADMIN));
+            adminUserController.create(new User(null, "userNameA", "emailA@mail.ru", "password", Role.ADMIN));
+            adminUserController.get(1);
+            System.out.println(adminUserController.getByMail("emailB@mail.ru"));
+            System.out.println(adminUserController.getAll());
+            adminUserController.update(new User(1, "userNameB", "emailB2@mail.ru", "password", Role.ADMIN),1);
+            System.out.println(adminUserController.get(1));
+            //System.out.println(adminUserController.getByMail("emailB@mail.ru"));
+            System.out.println(adminUserController.getAll());
         }
     }
 }
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -6,40 +6,78 @@ import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
+    private final Map<Integer, User> users = new ConcurrentHashMap<>();
+    private final Map<String, Integer> emails = new ConcurrentHashMap<>();
+    private final AtomicInteger counter = new AtomicInteger(0);
 
     @Override
     public boolean delete(int id) {
         log.info("delete {}", id);
-        return true;
+        User removedUser = users.remove(id);
+        return Objects.nonNull(emails.remove(removedUser.getEmail()));
     }
 
     @Override
     public User save(User user) {
         log.info("save {}", user);
-        return user;
+        if (user.isNew()) {
+            String email = user.getEmail();
+            checkEmail(email);
+            user.setId(counter.incrementAndGet());
+            users.put(user.getId(), user);
+            emails.put(email, user.getId());
+            return user;
+        }
+        // handle case: update, but not present in storage
+        return replace(user);
     }
 
     @Override
     public User get(int id) {
         log.info("get {}", id);
-        return null;
+        return users.get(id);
     }
 
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+        return users.values()
+                .stream()
+                .sorted(Comparator.comparing(User::getName, String.CASE_INSENSITIVE_ORDER))
+                .collect(Collectors.toList());
     }
 
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
+        Integer id = emails.get(email);
+        if (Objects.nonNull(id)) {
+            return users.get(id);
+        }
         return null;
+    }
+
+    private void checkEmail(String email) {
+        log.info("checkEmail {}", email);
+        if (Objects.nonNull((emails.get(email)))) {
+            throw new IllegalArgumentException(email + " already registered");
+        }
+    }
+
+    private User replace(User user) {
+        return users.computeIfPresent(user.getId(), (id, oldUser) -> {
+            if (Objects.isNull(emails.putIfAbsent(user.getEmail(), id))) {
+                emails.remove(oldUser.getEmail());
+            }
+            return user;
+        });
     }
 }
