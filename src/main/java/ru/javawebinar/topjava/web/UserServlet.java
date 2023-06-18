755f9de011c430a18331f9fc7ad1f10cfc297e9d
Alexandr Starodubenko <alexvladstr@gmail.com>
2023-06-18 17:24:03 +0300

    Implement selection of currently logged in user

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -18,4 +18,12 @@ public class UserServlet extends HttpServlet {
         log.debug("forward to users");
         request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("UTF-8");
+        String userId = request.getParameter("userId");
+        SecurityUtil.setAuthUserId(Integer.parseInt(userId));
+        response.sendRedirect("meals");
+    }
 }
