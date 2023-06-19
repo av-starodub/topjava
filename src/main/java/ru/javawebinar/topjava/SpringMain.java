4ce1996b17c46f1ec2bbd29f76a583cce62ca006
Alexandr Starodubenko <alexvladstr@gmail.com>
2023-06-19 22:37:23 +0300

    Fix InMemoryUserRepository according to code review

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -4,14 +4,11 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
-import ru.javawebinar.topjava.util.exception.NotFoundException;
 import ru.javawebinar.topjava.web.meal.MealRestController;
 import ru.javawebinar.topjava.web.user.AdminRestController;
 
 import java.util.Arrays;
-import java.util.Objects;
 
 public class SpringMain {
     public static void main(String[] args) {
@@ -28,26 +25,6 @@ public class SpringMain {
             MealRestController mealRestController = appCtx.getBean(MealRestController.class);
             MealsUtil.meals.forEach(mealRestController::create);
             mealRestController.get(1);
-
-            // Test email update
-            adminUserController.update(new User(1, "userName", "NEW@mail.ru", "password", Role.ADMIN), 1);
-            try {
-                adminUserController.getByMail("emailB@mail.ru");
-            } catch (NotFoundException e) {
-                User user = adminUserController.get(1);
-                System.out.println();
-                System.out.println("TEST: CheckEmailUpdate");
-                System.out.println("Email updated: " + "NEW@mail.ru".equals(user.getEmail()));
-                System.out.println("Expected NotFoundException :" + "Not found entity with email=emailB@mail.ru".equals(e.getMessage()));
-                System.out.println();
-            }
-
-            InMemoryUserRepository inMemoryUserRepository = appCtx.getBean(InMemoryUserRepository.class);
-            User actual = inMemoryUserRepository.getByEmail("emailB@mail.ru");
-            System.out.println();
-            System.out.println("TEST: CheckThatOldEmailRemoved");
-            System.out.println("Old email to user id relationship removed from relationship table: " + Objects.isNull(actual));
-            System.out.println();
         }
     }
 }
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -2,11 +2,11 @@ package ru.javawebinar.topjava.repository.inmemory;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.time.LocalDateTime;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -15,73 +15,56 @@ import java.util.stream.Collectors;
 @Repository
 public class InMemoryUserRepository implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
-    private final Map<Integer, User> users = new ConcurrentHashMap<>();
-    private final Map<String, Integer> emailsToUserIdRelationTable = new ConcurrentHashMap<>();
-    private final AtomicInteger counter = new AtomicInteger(0);
+    private final Map<Integer, User> repository;
+    private final AtomicInteger counter;
+
+    @Autowired
+    public InMemoryUserRepository() {
+        repository = new ConcurrentHashMap<>();
+        counter = new AtomicInteger(0);
+    }
 
     @Override
     public boolean delete(int id) {
-        log.info("{} delete {}", LocalDateTime.now(), id);
-        User removed = users.remove(id);
-        if (Objects.nonNull(removed)) {
-            emailsToUserIdRelationTable.remove(removed.getEmail());
-            return true;
-        }
-        return false;
+        log.info("delete {}", id);
+        return Objects.nonNull(repository.remove(id));
     }
 
     @Override
     public User save(User user) {
-        log.info("{} save {}", LocalDateTime.now(), user);
-        String email = user.getEmail();
-        if (isEmailRegistered(email)) {
-            return null;
-        }
+        log.info("save {}", user);
         if (user.isNew()) {
             user.setId(counter.incrementAndGet());
-            emailsToUserIdRelationTable.put(email, user.getId());
-            return users.put(user.getId(), user);
+            return repository.put(user.getId(), user);
         }
         // handle case: update, but not present in storage
-        return replace(user);
+        return repository.computeIfPresent(user.getId(), (id, oldUser) -> user);
     }
 
     @Override
     public User get(int id) {
-        log.info("{} get {}", LocalDateTime.now(), id);
-        return users.get(id);
+        log.info("get {}", id);
+        return repository.get(id);
     }
 
     @Override
     public List<User> getAll() {
-        log.info("{} getAll", LocalDateTime.now());
-        return users.values()
+        log.info("getAll");
+        return repository.values()
                 .stream()
-                .sorted(Comparator.comparing(User::getName, String.CASE_INSENSITIVE_ORDER))
+                .sorted(Comparator
+                        .comparing(User::getName, String.CASE_INSENSITIVE_ORDER)
+                        .thenComparing(User::getEmail, String.CASE_INSENSITIVE_ORDER)
+                )
                 .collect(Collectors.toList());
     }
 
     @Override
     public User getByEmail(String email) {
-        log.info("{} getByEmail {}", LocalDateTime.now(), email);
-        Integer id = emailsToUserIdRelationTable.get(email);
-        return Objects.nonNull(id) ? users.get(id) : null;
-    }
-
-    private boolean isEmailRegistered(String email) {
-        return Objects.nonNull(emailsToUserIdRelationTable.get(email));
-    }
-
-    private User replace(User user) {
-        return users.computeIfPresent(user.getId(), (id, oldUser) -> {
-            if (updateEmail(user)) {
-                emailsToUserIdRelationTable.remove(oldUser.getEmail());
-            }
-            return user;
-        });
-    }
-
-    private boolean updateEmail(User user) {
-        return Objects.isNull(emailsToUserIdRelationTable.putIfAbsent(user.getEmail(), user.getId()));
+        log.info("getByEmail {}", email);
+        return repository.values().stream()
+                .filter(user -> email.equalsIgnoreCase(user.getEmail()))
+                .findFirst()
+                .orElse(null);
     }
 }
